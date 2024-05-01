5f77fba8784e0d79a91c0ac9d50f753a9ef768a0
qwerty <qwerty@gmail.com>
2024-05-01 17:14:38 +0500

    secondDist

---
+++ dist/assets/MoviesPage-0v8mn_Nq.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Content-56dvjD9e.js";import{c as a,a as o,o as s}from"./index-Njscw5jx.js";const t={class:"main"},i={__name:"MoviesPage",setup(c){return(_,r)=>(s(),a("main",t,[o(e,{type:"movie"})]))}};export{i as default};
---
+++ dist/assets/TvsId-rlgWafQE.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ContentId-LQXf9fik.js";import{c as a,a as t,o as s}from"./index-Njscw5jx.js";const o={class:"main"},p={__name:"TvsId",setup(c){return(_,r)=>(s(),a("main",o,[t(e,{type:"tv"})]))}};export{p as default};
